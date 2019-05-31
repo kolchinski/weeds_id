3c5c88e55ca5c0ed71deecd7d1aaf51b6c2c1893
Alex Kolchinski <y.a.kolchinski@gmail.com>
2019-05-31 14:41:55 -0700

    Refactored model code

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from torchvision import datasets, models, transforms
 
 import data_util
 import constants
-from util import train_model
+from models.finetuned_resnet import FinetunedResnet
 
 np.set_printoptions(suppress=True)
 
@@ -25,29 +25,12 @@ tiny_run = True
 # Detect if we have a GPU available
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-model_class = models.resnet18 if tiny_run else models.resnet101
-model = model_class(pretrained=True)
+model_class = models.resnet18 if tiny_run else models.resnet50
 
-# Freeze the model parameters
-for param in model.parameters():
-    param.requires_grad = False
-
-last_layer_in_size = model.fc.in_features
-model.fc = nn.Linear(last_layer_in_size, constants.NUM_CLASSES)
-
-model.to(device)
-
-params_to_train = [p for p in model.parameters() if p.requires_grad]
-opt = optim.Adam(params_to_train, lr=constants.DEFAULT_INIT_LR)
-lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(opt, factor=0.5, patience=5, verbose=True) #TODO: parametrize, try cos
-print([p.shape for p in params_to_train])
-
-criterion = nn.CrossEntropyLoss()
 
 dataloaders = data_util.get_dataloaders(batch_size=constants.DEFAULT_BATCH_SIZE,
                                         augment_data=False,
                                         max_num_pts = 40 if tiny_run else None)
 
-# Train and evaluate
-model, hist = train_model(model, dataloaders, criterion,
-                          lr_scheduler, constants.MAX_EPOCHS, device, constants.NUM_CLASSES)
+resnet = FinetunedResnet(model_class, constants.NUM_CLASSES, device)
+resnet.train(dataloaders, constants.DEFAULT_INIT_LR, constants.MAX_EPOCHS)
---
+++ models/finetuned_resnet.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+import torch.optim as optim
+
+import constants
+from util import train_model
+
+
+class FinetunedResnet:
+    def __init__(self, resnet_class, num_classes, device):
+        model = resnet_class(pretrained=True)
+
+        for param in model.parameters():
+            param.requires_grad = False
+
+        last_layer_in_size = model.fc.in_features
+        model.fc = nn.Linear(last_layer_in_size, num_classes)
+
+        model.to(device)
+
+        self.model = model
+        self.device = device
+        self.num_classes = num_classes
+        self.resnet_class = resnet_class
+
+    def train(self, dataloaders, lr, max_epochs):
+        params_to_train = [p for p in self.model.parameters() if p.requires_grad]
+        opt = optim.Adam(params_to_train, lr=lr)
+        lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(opt, factor=0.5, patience=5, verbose=True) #TODO: parametrize, try cos
+        print([p.shape for p in params_to_train])
+
+        criterion = nn.CrossEntropyLoss()
+
+        model, hist = train_model(self.model, dataloaders, criterion,
+                          lr_scheduler, max_epochs, self.device, self.num_classes)
+
+        return model
+
